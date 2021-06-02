1a59055da5139a284f378e854a535108adb99158
Braulio <brauliomartinezlopez@hotmail.com>
2021-06-02 10:24:10 +0200

    deleted unnecesary things from home, articleWall, articleItem

---
+++ src/components/ArticleWall.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="delicious-section">
+
+    <div class="container-wrap">
+      <div class="columns is-gapless is-flex is-multiline">
+        <ArticleItem
+          v-for="item in articles"
+          :value="item"
+          :key="item._id"
+        />
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script>
+import ArticleItem from "@/components/partials/ArticleItem";
+
+export default {
+  name: "ArticleWall",
+  components: {
+    ArticleItem
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  computed: {
+  },
+  async beforeMount() {
+    try {
+      this.articles = await this.$api.articles.list();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/partials/ArticleItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="column is-4">
+    <div class="services-wrap is-flex">
+
+        <div class="text p-4">
+            <h3>{{value.title}}</h3>
+            <h4>{{value.published_at}}</h4>
+            <h4>{{value.category}}</h4>
+            <p>{{value.content}}</p>
+        </div>
+        
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "ArticleItem",
+    props: {
+        value: Object
+    }
+}
+</script>
